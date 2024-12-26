59f8bb98ff60752e185d9d9435030c27e659f56d
Vidith Madhu <vidhmadh1892@gmail.com>
2024-12-26 15:49:29 -0500

    Updated random matrix gen in select_demo

--- experimental/benchmark/select_demo.c
+++ experimental/benchmark/select_demo.c
@@ -2,11 +2,11 @@
 #include "LG_internal.h"
 #include "LAGraphX.h"
 
-#define DEFAULT_SIZE 500
-#define DEFAULT_DENSITY 0.7
+#define DEFAULT_SIZE 200000
+#define DEFAULT_DENSITY 0.07
 #define DEFAULT_SEED 42
 
-#define POSITIONAL 1
+#define POSITIONAL 0
 #define IJ 0
 #define BITMAP 0
 
@@ -32,26 +32,26 @@ int main(int argc, char **argv)
     uint64_t seed = (argc > 4 ? atoll (argv [4]) : DEFAULT_SEED) ;
 
     LG_TRY (LAGraph_Random_Init (msg)) ;
-    int ntrials = 10 ;
+    int ntrials = 1 ;
 
     for (int i = 0 ; i < ntrials ; i++) {
 
-        LG_TRY (LAGraph_Random_Matrix (&A, GrB_UINT64, n, n, density, seed, msg)) ;
+        LG_TRY (LAGraph_Random_Matrix (&A, GrB_FP64, n, n, density, seed, msg)) ;
         GRB_TRY (GxB_set (A, GxB_SPARSITY_CONTROL,
             BITMAP ? GxB_BITMAP : GxB_SPARSE)) ;
 
         GrB_Scalar s ;
-        GRB_TRY (GrB_Scalar_new (&s, GrB_UINT64)) ;
+        GRB_TRY (GrB_Scalar_new (&s, GrB_FP64)) ;
 
-        GRB_TRY (GrB_Matrix_new (&Res, GrB_UINT64, n, n)) ;
+        GRB_TRY (GrB_Matrix_new (&Res, GrB_FP64, n, n)) ;
 
         GrB_Index D_nvals, A_nvals ;
         GRB_TRY (GrB_Matrix_nvals (&A_nvals, A)) ;
         printf ("nvals: A: %ld\n", A_nvals) ;
 
-        GRB_TRY (GrB_Scalar_setElement_UINT64 (s, (uint64_t) 0)) ;
+        GRB_TRY (GrB_Scalar_setElement_UINT64 (s, (double) 0.5)) ;
         GRB_TRY (GrB_Matrix_select_Scalar (Res, NULL, NULL, (POSITIONAL ? 
-            (IJ ? GrB_DIAG : GrB_ROWLE) : GrB_VALUEEQ_UINT64), A, s, NULL)) ;
+            (IJ ? GrB_DIAG : GrB_ROWLE) : GrB_VALUELE_FP64), A, s, NULL)) ;
         
         GrB_Matrix_free (&A) ;
         GrB_Matrix_free (&Res) ;
